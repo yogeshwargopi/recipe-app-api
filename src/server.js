cb17828fdc74bca85e95b53033563d064cbd4478
yogesh-guvi-2913 <yogeshwar@guvi.in>
2025-10-19 18:23:29 +0530

    final bug cleared

--- src/server.js
+++ src/server.js
@@ -1,46 +1,46 @@
-import express from 'express';
-import { ENV } from './config/env.js';
-import { db } from './config/db.js';
+import express from "express";
+import { ENV } from "./config/env.js";
+import { db } from "./config/db.js";
+import { favoritesTable } from "./db/schema.js";
 import { and, eq } from "drizzle-orm";
-import { favoritesTable } from './db/schema.js';
 import job from "./config/cron.js";
 
 const app = express();
-const PORT = ENV.PORT;
+const PORT = ENV.PORT || 5001;
 
-if (ENV.NODE_ENV === 'production') {
-    job.start();
-}
+if (ENV.NODE_ENV === "production") job.start();
 
-app.get('/api/health', (req, res) => {
-    res.status(200).json({ status: 'OK' });
-})
+app.use(express.json());
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ success: true });
+});
 
 app.post("/api/favorites", async (req, res) => {
-    try {
-        const { userId, recipeId, title, image, cookTime, servings } = req.body;
+  try {
+    const { userId, recipeId, title, image, cookTime, servings } = req.body;
 
-        if (!userId || !recipeId || !title) {
-        return res.status(400).json({ error: "Missing required fields" });
-        }
-
-        const newFavorite = await db
-        .insert(favoritesTable)
-        .values({
-            userId,
-            recipeId,
-            title,
-            image,
-            cookTime,
-            servings,
-        })
-        .returning();
-
-        res.status(201).json(newFavorite[0]);
-    } catch (error) {
-        console.log("Error adding favorite", error);
-        res.status(500).json({ error: "Something went wrong" });
+    if (!userId || !recipeId || !title) {
+      return res.status(400).json({ error: "Missing required fields" });
     }
+
+    const newFavorite = await db
+      .insert(favoritesTable)
+      .values({
+        userId,
+        recipeId,
+        title,
+        image,
+        cookTime,
+        servings,
+      })
+      .returning();
+
+    res.status(201).json(newFavorite[0]);
+  } catch (error) {
+    console.log("Error adding favorite", error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
 });
 
 app.get("/api/favorites/:userId", async (req, res) => {
@@ -76,6 +76,6 @@ app.delete("/api/favorites/:userId/:recipeId", async (req, res) => {
   }
 });
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port: ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log("Server is running on PORT:", PORT);
+});
