13266895827b200f89914bedc3280ed0c5fb5ded
yogesh-guvi-2913 <yogeshwar@guvi.in>
2025-10-19 13:30:01 +0530

    added flow for cron job

--- src/server.js
+++ src/server.js
@@ -2,9 +2,14 @@ import express from 'express';
 import { ENV } from './config/env.js';
 import { db } from './config/db.js';
 import { favoritesTable } from './db/schema.js';
+import job from "./config/cron.js";
 
 const app = express();
 const PORT = ENV.PORT;
+
+if (ENV.NODE_ENV === 'production') {
+    job.start();
+}
 
 app.get('/api/health', (req, res) => {
     res.status(200).json({ status: 'OK' });
